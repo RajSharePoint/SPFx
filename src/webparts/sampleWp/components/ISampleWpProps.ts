5504a12b441206f66de0dcdda5447fd2cdd56663
raj <bokaroman@gmail.com>
2026-07-12 14:05:38 +0530

    initial code

---
+++ src/webparts/sampleWp/components/ISampleWpProps.ts
@@ -0,0 +1,6 @@
+export interface ISampleWpProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  userDisplayName: string;
+}
---
+++ src/webparts/sampleWp/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISampleWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SampleWpWebPartStrings' {
+  const strings: ISampleWpWebPartStrings;
+  export = strings;
+}
